9af1289662dbb98537b7c341d63e88695b56b63d
nnakata <nakata@team-lab.com>
2013-10-18 20:40:49 +0900

    PlayScheduled 使うように変更

--- Assets/Scripts/UtcClock.cs
+++ Assets/Scripts/UtcClock.cs
@@ -3,48 +3,63 @@ using System.Collections;
 
 public class UtcClock : MonoBehaviour {
 	public const float MILS2ANGLE = 360f / 1000f;
+	public const long SECOND_IN_TICKS = 10000000L;
+	public const long MILLISECOND_IN_TICKS = 10000L;
+	public const double TICKS2SECOND = 1e-7;
 	
 	public OscNtpClient client;
 	public GUIText text;
 	public Transform clock;
-	public AudioSource beat;
+	public BeatInfo[] beats;
 	
 	private bool _synch = true;
 
 	void Start () {
 		if (text == null)
 			text = guiText;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		UpdateTime();
+		
+		var dspEndTime = AudioSettings.dspTime;
+		foreach (var bi in beats) {
+			bi.source = gameObject.AddComponent<AudioSource>();
+			bi.source.bypassEffects = true;
+			bi.source.clip = bi.clip;
+			bi.dspEndTime = dspEndTime;
+		}
 	}
 	
 	void OnGUI() {
 		_synch = GUILayout.Toggle(_synch, "Sync");
 	}
 
-	void UpdateTime() {
+	void Update() {
 		var now = HighResTime.UtcNow;
 		var delay = client.AverageDelay();
 		if (_synch)
 			now = now.AddSeconds(delay);
 		
-		var playPos = now.Millisecond * 1.0e-3f;
-		if (playPos < beat.clip.length)
-			Play(playPos);
-		
 		text.text = string.Format("{0}\nDelay {1:E3}", now, delay);
 		var angle = MILS2ANGLE * now.Millisecond;
 		clock.localRotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+		
+		var dspTime = AudioSettings.dspTime;
+		foreach (var bi in beats) {
+			if (bi.dspEndTime < dspTime) {
+				var intervalInTicks = bi.intervalInMills * MILLISECOND_IN_TICKS;
+				var dt = (intervalInTicks - (now.Ticks % intervalInTicks)) *TICKS2SECOND;
+				var startDspTime = dspTime + dt;
+				bi.dspEndTime = startDspTime + bi.source.clip.length;
+				bi.source.PlayScheduled(startDspTime);
+			}
+		}
 	}
 	
-	void Play(float seek) {
-		if (beat.isPlaying)
-			return;
-		
-		beat.time = seek;
-		beat.Play();
+	[System.Serializable]
+	public class BeatInfo {
+		public int intervalInMills = 1000;
+		public AudioClip clip;
+		[HideInInspector]
+		public AudioSource source;
+		[HideInInspector]
+		public double dspEndTime;
 	}
 }
